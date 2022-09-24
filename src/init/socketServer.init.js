acf89e106b8b858d3320c5e006036c195361b499
TankistPro <nakol.nayk@mail.ru>
2022-09-25 00:04:08 +0300

    fix

--- src/init/socketServer.init.js
+++ src/init/socketServer.init.js
@@ -9,7 +9,7 @@ const PORT = 5605;
 const server = http.createServer(expressApp);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: '*',
         credentials: true
     }
 });
